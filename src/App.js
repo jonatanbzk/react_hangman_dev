a089e4fc59f9aea8d471266253737e20fbee5735
Buzek Jonatan <buzek.jonatan@gmail.com>
2020-01-07 14:42:09 +0100

    keyboard works - loop on word compo works - array with clicked letter works - commit before create just one class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from './button.js';
+//import Button from './button.js';
 import './App.css';
 
 const lettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
@@ -7,18 +7,59 @@ const lettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
 
 const word = ["F", "U", "C", "K"]
 
+const clickedKeys = []
+
+class Button extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: ""
+    }
+  }
+
+  onClick() {
+    this.setState({color: 'color'});
+    if (!clickedKeys.includes(this.props.value)) {
+      clickedKeys.push(this.props.value);
+    }
+    console.log(clickedKeys);
+  }
+
+  render(){
+    let btn_class = this.state.color ? 'color' : ''
+
+    return (
+      <div className="touche">
+        <button className={btn_class}
+                value=""
+                onClick={this.onClick.bind(this)}>
+                {this.props.value}
+        </button>
+      </div>
+    )
+  }
+}
+
+
 class App extends React.Component {
+
   render(){
     return(
    <React.Fragment>
       <div className="wordBloc">
        {word.map((letter) => (
-	   <div className={"wordLetter"}> {letter} </div>
-	))}
+	        <div className={"wordLetter"} key={letter}>
+            __
+          </div>
+	        ))}
       </div>
+
       <div className="keyboard">
-        {lettersArr.map((key) => (
-            <Button value={key}>  </ Button>
+        {lettersArr.map((touche, index) => (
+            <Button
+              key={index}
+              value={touche}>
+            </ Button>
         ))}
       </div>
    </React.Fragment>
