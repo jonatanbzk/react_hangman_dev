2f0b397ba0c0ddfcc7d2cf61976475252e0a8071
Buzek Jonatan <buzek.jonatan@gmail.com>
2020-01-10 08:47:26 +0100

    hangMan works only left change initial word

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
+import Words from './Words.js';
 
 const lettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
 "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+/*
+const wordSelect = Words[Math.floor(Math.random() * Math.floor(848))].toUpperCase().split("")
+*/
 
-const word = ["H", "E", "L", "L", "O"]
-
-
-const Button = ({ touche, onClick, value }) => (
-
+const BUTTON = ({ touche, onClick, value, feedback}) => (
   <div className="touche">
-    <button onClick={() => onClick(touche)}>
+    <button className={feedback} onClick={() => onClick(touche)}>
             {value}
     </button>
   </div>
@@ -20,26 +20,47 @@ class App extends React.Component {
   constructor(props) {
   super(props)
   this.state = {
-    color: "",
-    clickedKeys: ["X"],
+    randNbr: 0,
+    clickedKeys: [],
+    attempt: 0,
+    goodAttempt: 0,
   }
 }
+    getWord() {
+      const { randNbr } = this.state
+      this.setState({ randNbr: Math.floor(Math.random() * Math.floor(848)) })
+      return Words[this.state.randNbr].toUpperCase().split("")
+    }
 
+    getFeedBack(index) {
+      const { clickedKeys } = this.state
+      if (clickedKeys.includes(index)) {
+        return 'color'
+      }
+    }
 
     handleKeyClick = touche => {
-    const { clickedKeys, color } = this.state
-    this.setState({color: 'color'});
+    const { clickedKeys, attempt, goodAttempt } = this.state
     if (!this.state.clickedKeys.includes(touche)) {
       this.setState({ clickedKeys: [...clickedKeys, ...touche] })
+      this.setState({ attempt: attempt +1 })
     }
-    console.log(touche);
-    console.log(this.state.clickedKeys);
-    console.log(this.state.color)
+    if (!this.state.clickedKeys.includes(touche) && word.includes(touche)) {
+      let count = word.filter(w => w === touche).length
+      this.setState({ goodAttempt: goodAttempt +count })
+    }
+    console.log(wordSelect)
   }
 
-  render(){
-    let btn_class = this.state.color ? 'color' : ''
+    restartGame = () => {
+      const { clickedKeys, attempt, goodAttempt } = this.setState
+      this.setState({ attempt: 0 })
+      this.setState({ goodAttempt: 0 })
+      this.setState({ clickedKeys: [] })
+    }
 
+  render(){
+    const won = this.state.goodAttempt === word.length
     return(
    <React.Fragment>
       <div className="wordBloc">
@@ -49,22 +70,34 @@ class App extends React.Component {
           </div>
 	        ))}
       </div>
-
+      {!won ?
       <div className="keyboard">
         {lettersArr.map((touche, index) => (
-            <Button
+            <BUTTON
               touche={touche}
-              key={index}
+              feedback={this.getFeedBack(touche)}
+              key={touche}
               value={touche}
               onClick={this.handleKeyClick}>
               {touche}
-            </ Button>
+            </ BUTTON>
         ))}
-      </div>
+      </div> :
+        <p>
+          <button type="button"
+                  id="restartGame"
+                  onClick={this.restartGame}>
+                  New Game
+          </button>
+        </p>
+      }
+     <div className="score">
+      <br />
+      Attempt : {this.state.attempt} <br />
+     </div>
    </React.Fragment>
     )
   }
-
 }
 
 export default App;
